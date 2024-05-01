bf9ab7d7ed6c86816f07d81f7dda74cb8f645765
Nevers <123786017+AlwaysNevers@users.noreply.github.com>
2024-05-01 12:48:27 -0700

    Update movies.js

--- movies.js
+++ movies.js
@@ -1,190 +1,188 @@
 document.getElementById('searchForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the default form submission
-    searchMovies();
-  });
+  event.preventDefault(); // Prevent the default form submission
+  searchMovies();
+});
 
 document.getElementById('searchbar').addEventListener('input', function(event) {
-const searchTerm = event.target.value.trim();
-if (searchTerm === '') {
+  const searchTerm = event.target.value.trim();
+  if (searchTerm === '') {
     showCategories(); // Show categories if the search bar is empty
-} else {
+  } else {
     hideCategories(); // Hide categories if the user starts typing
-}
-searchMovies(); // Search movies as the user types
+  }
+  searchMovies(); // Search movies as the user types
 });
 
 function showCategories() {
-const categoryContainers = document.querySelectorAll('.category-container');
-categoryContainers.forEach(container => {
+  const categoryContainers = document.querySelectorAll('.category-container');
+  categoryContainers.forEach(container => {
     container.style.display = 'block';
-});
+  });
 }
 
 function hideCategories() {
-const categoryContainers = document.querySelectorAll('.category-container');
-categoryContainers.forEach(container => {
+  const categoryContainers = document.querySelectorAll('.category-container');
+  categoryContainers.forEach(container => {
     container.style.display = 'none';
-});
+  });
 }
 
 async function searchMovies() {
-try {
+  try {
     const searchTerm = document.getElementById('searchbar').value.trim();
-    if (searchTerm === '') {
-        clearMovies();
-        return;
-    }
     const apiKey = 'c0aa80b5561bd96b6cd261ac6d57886c';
-    const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchTerm}`);
-    const data = await response.json();
-
-    console.log(data); // Log the response for debugging
-
-    if (data.results.length > 0) {
-        // Clear "No results found" message
-        document.getElementById('noResults').innerText = '';
-        // Display the movies
-        displayMovies(data.results, 'movieContainer');
-    } else {
-        // Display "No results found!" text on the screen
-        document.getElementById('noResults').innerText = 'No results found';
-        // Clear any existing movies from the screen
-        clearMovies();
+    if (searchTerm === '') {
+      clearMovies();
+      return; // Exit the function early if search term is empty
     }
-} catch (error) {
-    console.error('Error fetching movies:', error);
-    alert('An error occurred while fetching movies.');
-}
-}
 
+    // Search for both movies and TV shows
+    const movieResponse = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchTerm}`);
+    const tvResponse = await fetch(`https://api.themoviedb.org/3/search/tv?api_key=${apiKey}&query=${searchTerm}`);
+    const movieData = await movieResponse.json();
+    const tvData = await tvResponse.json();
 
-function displayMovies(movies, containerId) {
-const movieContainer = document.getElementById(containerId);
-movieContainer.innerHTML = ''; // Clear previous results
+    // Combine movie and TV show results
+    const combinedResults = [...movieData.results, ...tvData.results];
 
-movies.forEach(movie => {
-if (movie.poster_path) {
-const movieItem = document.createElement('div');
-movieItem.classList.add('movie-item');
-
-const posterPath = `https://image.tmdb.org/t/p/w200${movie.poster_path}`;
-const img = document.createElement('img');
-img.src = posterPath;
-movieItem.appendChild(img);
-
-movieItem.addEventListener('click', () => redirectToTMDb(movie.id));
-movieContainer.appendChild(movieItem); // Append to the respective category container
-}
-});
-}
-
-function displayErrorPopup(message) {
-    console.log('displayErrorPopup called');
-    const popup = document.createElement('div');
-    popup.classList.add('error-popup');
-    popup.textContent = message;
-
-    const closeButton = document.createElement('button');
-    closeButton.textContent = 'Close';
-    closeButton.addEventListener('click', function() {
-        popup.remove();
-    });
-
-    popup.appendChild(closeButton);
-    document.body.appendChild(popup);
-
-    console.log('Error popup displayed');
-}
-
-function redirectToTMDb(movieId) {
-    const loading = document.querySelector('.lds-ring'); // Get the loading animation element
-    loading.style.display = 'block'; // Display the loading animation
-
-    const movieUrl = `https://vidsrc.to/embed/movie/${movieId}`;
-    const iframe = document.createElement("iframe");
-    iframe.src = movieUrl;
-    iframe.style.width = "100%";
-    iframe.style.height = "100%";
-    iframe.style.border = "0";
-    iframe.style.position = "fixed";
-    iframe.style.top = "0";
-    iframe.style.left = "0";
-    iframe.referrerPolicy = "no-referrer";
-    iframe.allow = "fullscreen";
-    
-    setTimeout(function() {
-        document.getElementById("movieContainer").innerHTML = ""; // Clear previous content
-        document.getElementById("movieContainer").appendChild(iframe);
-        loading.style.display = 'none'; // Hide loading animation when iframe content is loaded
-    }, 100); // Adjust time delay as needed (100 milliseconds)
-
-    displayErrorPopup('Close any new tabs that open! Theyre just third party ads and I cant get rid of them :(');
+    if (combinedResults.length > 0) {
+      // Clear "No results found" message
+      document.getElementById('noResults').innerText = '';
+      // Display the movies and TV shows
+      displayMovies(combinedResults, 'movieContainer');
+    } else {
+      // Display "No results found!" text on the screen
+      document.getElementById('noResults').innerText = 'No results found';
+      // Clear any existing movies from the screen
+      clearMovies();
+    }
+  } catch (error) {
+    console.error('Error fetching movies and TV shows:', error);
+    // Prevent alert when clearing search bar
+    const searchTerm = document.getElementById('searchbar').value.trim();
+    if (searchTerm !== '') {
+      alert('An error occurred while fetching movies and TV shows.');
+    }
+  }
 }
 
 function clearMovies() {
   document.getElementById('movieContainer').innerHTML = '';
 }
 
+function displayMovies(media, containerId) {
+  const mediaContainer = document.getElementById(containerId);
+  mediaContainer.innerHTML = ''; // Clear previous results
+
+  media.forEach(item => {
+    const mediaItem = document.createElement('div');
+    mediaItem.classList.add('movie-item');
+
+    const posterPath = `https://image.tmdb.org/t/p/w200${item.poster_path}`;
+    const img = document.createElement('img');
+    img.src = posterPath;
+    mediaItem.appendChild(img);
+
+    // Determine media type based on endpoint
+    let mediaType;
+    if (item.hasOwnProperty('title')) {
+      mediaType = 'movie';
+    } else if (item.hasOwnProperty('name')) {
+      mediaType = 'tv';
+    } else {
+      mediaType = 'unknown'; // Handle unknown media types
+    }
+
+    mediaItem.addEventListener('click', () => redirectToTMDb(item.id, mediaType));
+    mediaContainer.appendChild(mediaItem); // Append to the respective category container
+  });
+}
+
+function redirectToTMDb(mediaId, mediaType) {
+  const loading = document.querySelector('.lds-ring'); // Get the loading animation element
+  loading.style.display = 'block'; // Display the loading animation
+
+  let mediaUrl;
+  if (mediaType === 'movie') {
+    mediaUrl = `https://vidsrc.to/embed/movie/${mediaId}`;
+  } else if (mediaType === 'tv') {
+    mediaUrl = `https://vidsrc.to/embed/tv/${mediaId}`; // Correct URL for TV shows
+  }
+
+  const iframe = document.createElement("iframe");
+  iframe.src = mediaUrl;
+  iframe.style.width = "100%";
+  iframe.style.height = "100%";
+  iframe.style.border = "0";
+  iframe.style.position = "fixed";
+  iframe.style.top = "0";
+  iframe.style.left = "0";
+  iframe.referrerPolicy = "no-referrer";
+  iframe.allow = "fullscreen";
+
+  setTimeout(function() {
+    document.getElementById("movieContainer").innerHTML = ""; // Clear previous content
+    document.getElementById("movieContainer").appendChild(iframe);
+    loading.style.display = 'none'; // Hide loading animation when iframe content is loaded
+  }, 100); // Adjust time delay as needed (100 milliseconds)
+
+  alert('Close any new tabs that open! They\'re just third-party ads and I can\'t get rid of them :(');
+}
+
 // Load popular, recent, and highly rated movies
 document.addEventListener('DOMContentLoaded', async function() {
-await loadPopularMovies();
-await loadRecentMovies();
-await loadHighlyRatedMovies();
+  await loadPopularMovies();
+  await loadRecentMovies();
+  await loadHighlyRatedMovies();
 });
 
 async function loadPopularMovies() {
-try {
+  try {
     const apiKey = 'c0aa80b5561bd96b6cd261ac6d57886c';
     const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`);
     const data = await response.json();
 
-    console.log(data); // Log the response for debugging
-
     if (data.results.length > 0) {
-        displayMovies(data.results, 'popularMovies');
+      displayMovies(data.results, 'popularMovies');
     } else {
-        alert('No popular movies found!');
+      alert('No popular movies found!');
     }
-} catch (error) {
+  } catch (error) {
     console.error('Error fetching popular movies:', error);
     alert('An error occurred while fetching popular movies.');
-}
+  }
 }
 
 async function loadRecentMovies() {
-try {
+  try {
     const apiKey = 'c0aa80b5561bd96b6cd261ac6d57886c';
     const response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}`);
     const data = await response.json();
 
-    console.log(data); // Log the response for debugging
-
     if (data.results.length > 0) {
-        displayMovies(data.results, 'recentMovies');
+      displayMovies(data.results, 'recentMovies');
     } else {
-        alert('No recent movies found!');
+      alert('No recent movies found!');
     }
-} catch (error) {
+  } catch (error) {
     console.error('Error fetching recent movies:', error);
     alert('An error occurred while fetching recent movies.');
-}
+  }
 }
 
 async function loadHighlyRatedMovies() {
-try {
+  try {
     const apiKey = 'c0aa80b5561bd96b6cd261ac6d57886c';
     const response = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`);
     const data = await response.json();
 
-    console.log(data); // Log the response for debugging
-
     if (data.results.length > 0) {
-        displayMovies(data.results, 'highRatedMovies');
+      displayMovies(data.results, 'highRatedMovies');
     } else {
-        alert('No highly rated movies found!');
+      alert('No highly rated movies found!');
     }
-} catch (error) {
+    } catch (error) {
     console.error('Error fetching highly rated movies:', error);
     alert('An error occurred while fetching highly rated movies.');
-}
-}
+    }
+    }
